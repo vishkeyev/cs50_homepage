959c8ea562a99bb648c0e93fd3e91808b9123f60
vishkeyev <79332218+vishkeyev@users.noreply.github.com>
2022-07-17 03:49:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var myModal = new bootstrap.Modal(document.getElementById('MyModal'), { keyboard: false });
 document.addEventListener('DOMContentLoaded', function () {
-    //alert("Welcome!!!");
     myModal.show();
 });
